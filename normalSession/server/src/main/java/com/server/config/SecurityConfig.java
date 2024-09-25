f784c737d9fc4dfa03010e6ad0a18dfea55cc769
jeoungho.yoo <jeoungho.yoo@stans.co.kr>
2024-09-25 17:45:13 +0900

    충돌 수정

--- normalSession/server/src/main/java/com/server/config/SecurityConfig.java
+++ normalSession/server/src/main/java/com/server/config/SecurityConfig.java
@@ -1,57 +1,33 @@
 package com.server.config;
 
 import lombok.extern.log4j.Log4j2;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Component;
 
 @Component
 @Log4j2
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // ℹ️ CSRF 미사용
+        http.csrf(csrf -> csrf.disable());
 
-        http.formLogin(formLogin->{
-            formLogin.loginPage("/loginForm");
-
-            // ℹ️ Spring Security에서는 따로 Long form을 사용할 경우 Controller 불필요
+        // ℹ️ Form 설정
+        http.formLogin(formLogin -> {
             formLogin.loginProcessingUrl("/login");
-            
-            // 로그인이 끝나면 리다이렉트할 url
-		    formLogin.defaultSuccessUrl("/");
         });
 
-        // 모든 접근 제한
-        http.authorizeHttpRequests( access ->{
+        // 접근 제한 추가
+        http.authorizeHttpRequests(access -> {
+            access.requestMatchers("/login").permitAll();
             access.anyRequest().authenticated();
         });
+
         return http.build();
     }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        // 인메모리 사용자 저장소. 실제 서비스에서는 DB를 사용할 것을 권장
-        User.UserBuilder users = User.withDefaultPasswordEncoder();
-        return new InMemoryUserDetailsManager(
-                users.username("user").password("123").roles("USER").build(),
-                users.username("admin").password("123").roles("ADMIN").build()
-        );
-    }
-
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer(){
-        return web -> web.ignoring()
-                .requestMatchers(HttpMethod.POST,"/login")
-                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
-    }
-
-
 }
