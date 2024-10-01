4010f9660e99ab7bc4e23aa28493be37e925c018
yoo <edel1212@naver.com>
2024-10-01 17:28:21 +0900

    CRSF Token 컨트롤러 추가

---
+++ normalSession/server/src/main/java/com/server/controller/CsrfController.java
@@ -0,0 +1,21 @@
+package com.server.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class CsrfController {
+
+    @GetMapping("/csrf")
+    public Map<String, String> getCsrfToken(HttpServletRequest request) {
+        CsrfToken csrfToken = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+        Map<String, String> tokenMap = new HashMap<>();
+        tokenMap.put("token", csrfToken.getToken());
+        return tokenMap;
+    }
+}
