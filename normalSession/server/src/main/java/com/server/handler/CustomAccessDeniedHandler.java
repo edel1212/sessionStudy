23ad1ce1fed78e52eec308d5e24c8e4860c4299d
yoo <edel1212@naver.com>
2024-10-01 16:44:31 +0900

    log 추가

--- normalSession/server/src/main/java/com/server/handler/CustomAccessDeniedHandler.java
+++ normalSession/server/src/main/java/com/server/handler/CustomAccessDeniedHandler.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
@@ -19,8 +20,9 @@ import java.util.Map;
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
 
     @Override
-    public void handle(HttpServletRequest request, HttpServletResponse response, org.springframework.security.access.AccessDeniedException accessDeniedException) throws IOException, ServletException {
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
         log.info("- Custom Access Denied Handler 접근 -");
+        log.info(accessDeniedException.getMessage());
         var objectMapper = new ObjectMapper();
         int scUnauthorized = HttpServletResponse.SC_UNAUTHORIZED;
         response.setStatus(scUnauthorized);
