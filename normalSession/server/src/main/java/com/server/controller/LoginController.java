dd5de98625b9bbb407788bbed4730825419690d7
yoo <edel1212@naver.com>
2024-10-07 22:01:19 +0900

    import 추가

--- normalSession/server/src/main/java/com/server/controller/LoginController.java
+++ normalSession/server/src/main/java/com/server/controller/LoginController.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
