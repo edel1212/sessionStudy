fa734bfc4d5768c79b439f34645903e2092238fb
yoo <edel1212@naver.com>
2024-10-20 13:55:47 +0900

    Init Project ;

---
+++ redisSession/server-header/src/main/java/com/yoo/server_header/ServerHeaderApplication.java
@@ -0,0 +1,13 @@
+package com.yoo.server_header;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerHeaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerHeaderApplication.class, args);
+	}
+
+}
---
+++ redisSession/server-header/src/test/java/com/yoo/server_header/ServerHeaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoo.server_header;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerHeaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
