d807ed95ed77cb81148515b453f2516c27ae4c8e
yoo <edel1212@naver.com>
2024-10-20 17:34:52 +0900

    Sessiong Header 진행중

---
+++ client/src/app/redis-header-session/page.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import { useState } from "react";
+
+export default function RedisHeaderSession() {
+  const [responseData, setResponseData] = useState(""); // 응답 데이터를 저장할 상태 추가
+  const [xAtuhToken, setXAuthToken] = useState("");
+
+  const apiResponse = async (url: string) => {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "X-auth-token": xAtuhToken,
+      },
+      credentials: "include",
+    });
+    const data = await response.json();
+    if (response.ok) {
+      console.log("요청 성공:", data);
+      setResponseData(JSON.stringify(data, null, 2)); // JSON 데이터를 문자열로 변환하여 상태에 저장
+    } else {
+      console.error("요청 실패");
+      setResponseData("Error: 요청에 실패했습니다."); // 오류 메시지 출력
+    }
+  };
+
+  const logOut = async () => {
+    fetch("http://localhost:8080/member/logout", {
+      method: "POST",
+      headers: {
+        "X-auth-token": xAtuhToken,
+      },
+      credentials: "include", // 쿠키/세션 정보 포함
+    }).then((response) => {
+      if (response.ok) {
+        alert("로그아웃 성공!");
+        console.log("Logged out successfully");
+        setResponseData("Logged out successfully"); // 로그아웃 결과 출력
+      }
+    });
+  };
+
+  const logIn = async () => {
+    const formData = new URLSearchParams();
+    formData.append("username", "yoo");
+    formData.append("password", "123");
+
+    const response = await fetch("http://localhost:8080/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: formData.toString(),
+    });
+    const data = await response.json();
+    console.log(data);
+    if (response.ok) {
+      console.log("로그인 성공:", data);
+      setResponseData(JSON.stringify(data, null, 2));
+      // Session Token 값 반환
+      setXAuthToken(data.xAuthToken);
+    } else {
+      console.error("로그인 실패");
+      setResponseData("Error: 로그인에 실패했습니다."); // 오류 메시지 출력
+    }
+  };
+
+  return (
+    <main style={{ display: "flex", justifyContent: "space-between" }}>
+      <section style={{ width: "100vw", border: "1px solid" }}>
+        <p>
+          <button onClick={() => apiResponse("http://localhost:8080/all")}>
+            All Access
+          </button>
+        </p>
+        <p>
+          <button onClick={() => apiResponse("http://localhost:8080/no-login")}>
+            No Login
+          </button>
+        </p>
+        <p>
+          <button
+            onClick={() => apiResponse("http://localhost:8080/has-certified")}
+          >
+            Login User
+          </button>
+        </p>
+        <p>
+          <button onClick={() => apiResponse("http://localhost:8080/admin")}>
+            Admin
+          </button>
+        </p>
+        <p>
+          <button onClick={() => apiResponse("http://localhost:8080/user")}>
+            User
+          </button>
+        </p>
+        <hr />
+        <p>
+          <button onClick={() => logIn()}>Log-In</button>
+        </p>
+        <p>
+          <button onClick={() => logOut()}>Log-Out</button>
+        </p>
+      </section>
+      <section style={{ width: "100vw", border: "1px solid" }}>
+        <textarea
+          style={{ width: "50vw", height: "50vh" }}
+          value={responseData} // 상태값을 textarea에 표시
+          readOnly
+        ></textarea>
+      </section>
+    </main>
+  );
+}
--- redisSession/server-header/src/main/java/com/yoo/server_header/controller/LoginController.java
+++ redisSession/server-header/src/main/java/com/yoo/server_header/controller/LoginController.java
@@ -4,6 +4,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -24,47 +26,48 @@ import java.util.Map;
 public class LoginController {
     // Spring Security Manager
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final RedisTemplate redisTemplate;
+
+    @Value("${spring.session.redis.namespace}")
+    private String redisSessionPrefix;
 
     @PreAuthorize("isAnonymous()")
     @PostMapping("/login")
     public ResponseEntity<Map<String, String>> login(String username, String password, HttpServletRequest request) {
+        // 인증 로직
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
-
         Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
 
         // 인증 정보를 SecurityContext에 저장
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
+        // Redis에서 기존 세션 찾기
+        String existingSessionId = (String) redisTemplate.opsForValue().get("loginUserId:" + username);
+        log.info("--------------");
+        log.info(existingSessionId);
+        log.info("--------------");
+        // 이미 로그인한 경우
+        if (existingSessionId != null) {
+            // 기존 세션 무효화
+//            HttpSession oldSession = request.getSession(false);
+//            oldSession.invalidate();
+            // Redis에 저장된 Session 제거
+            log.info("redisSessionPrefix ::" + redisSessionPrefix + ":sessions:" +existingSessionId);
+            // TOdo 체크 필요
+            redisTemplate.delete(redisSessionPrefix + ":sessions:" +existingSessionId);
+        }// if
+
         // 새로운 세션 생성
-        HttpSession session = request.getSession(true);
-        session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, SecurityContextHolder.getContext());
+        HttpSession newSession = request.getSession(true);
+        newSession.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, SecurityContextHolder.getContext());
+
+        // Redis에 새 세션 ID 저장
+        redisTemplate.opsForValue().set("loginUserId:" + username, newSession.getId());
 
         Map<String, String> result = new HashMap<>();
         result.put("userName", authentication.getName());
-        result.put("sessionToken", HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
+        result.put("sessionToken", newSession.getId());
 
-        return ResponseEntity.ok(result);
-    }
-
-
-    @GetMapping("/add")
-    public ResponseEntity<?> getSessionData(HttpServletRequest request) {
-        // 새로운 세션 생성
-        HttpSession session = request.getSession(true);
-        session.setAttribute("yoo", "yooJh");
-        return ResponseEntity.ok("good");
-    }
-
-    @GetMapping("/check")
-    public ResponseEntity<?> addSession(HttpServletRequest request) {
-        String sessionId = request.getHeader("sessionToken");
-        log.info("-------------------------");
-        log.info(sessionId);
-        log.info("-------------------------");
-        String sessionData = (String) request.getSession(false).getAttribute(sessionId);
-        log.info(sessionData);
-        Map<String, String > result = new HashMap<>();
-        result.put("result",sessionData);
         return ResponseEntity.ok(result);
     }
 
