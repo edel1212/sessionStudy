595a24ef5bf86d076fe8110921bc33fa6d22b687
edel1212 <50935771+edel1212@users.noreply.github.com>
2024-10-21 17:34:41 +0900

    x-auth-token 값 확인

--- redisSession/server-header/src/main/java/com/yoo/server_header/filter/SessionCheckFilter.java
+++ redisSession/server-header/src/main/java/com/yoo/server_header/filter/SessionCheckFilter.java
@@ -1,4 +1,31 @@
 package com.yoo.server_header.filter;
 
-public class SessionCheckFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Log4j2
+@Component
+@RequiredArgsConstructor
+public class SessionCheckFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String sessionId = request.getHeader("x-auth-token");
+        log.info("--------------");
+        log.info("x-auth-token :: " + sessionId );
+        log.info("--------------");
+        // session이 없을 경우 해당 필터 스킵
+        if(sessionId == null){
+            filterChain.doFilter(request, response);
+            return;
+        }// if
+        
+    }
 }
