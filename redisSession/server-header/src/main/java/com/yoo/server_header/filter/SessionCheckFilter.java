ec2bd7823dec8b47879b3409c6344294daa20607
edel1212 <50935771+edel1212@users.noreply.github.com>
2024-10-21 18:22:21 +0900

    SecurityContextHolder 지정 로직 추가  - 테스트 완료

--- redisSession/server-header/src/main/java/com/yoo/server_header/filter/SessionCheckFilter.java
+++ redisSession/server-header/src/main/java/com/yoo/server_header/filter/SessionCheckFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.session.Session;
 import org.springframework.session.data.redis.RedisSessionRepository;
@@ -24,28 +25,41 @@ public class SessionCheckFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // 1. Get Header 값
         String sessionId = request.getHeader("x-auth-token");
-        log.info("--------------");
-        log.info("x-auth-token :: " + sessionId );
-        log.info("--------------");
+
+        // Header 값이 없을 경우 Skip
         if(sessionId == null){
             filterChain.doFilter(request, response);
             return;
         }// if
 
-        // 1. Get the session from Redis
+        // 2. sessionId을 사용 Redis에서 Session 존재 여부 확인
         Session session = redisSessionRepository.findById(sessionId);
 
-        // 2. Check if session exists
+        // 3. Check if session exists
         if (session != null) {
+            
             // Log ::  Session found in Redis: 5844cdce-cdb0-4f6c-9640-ac865eaafda4
             log.info("Session found in Redis: " + sessionId);
-            // Get Security Context on Redis session
+
+
+            // 3-1. Session 내 SecurityContext 값 추출
             SecurityContext securityContext = (SecurityContext) session.getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
+
+            //  Check SecurityContext exists
             if (securityContext != null) {
+                
                 // Log :: Authenticated user: yoo
                 log.info("Authenticated user: " + securityContext.getAuthentication().getName());
-            } // if
+                
+                // ✅ SecurityContextHolder에 값을 주입 - 가장 중요 로직
+                SecurityContextHolder.setContext(securityContext);
+                
+            }  else {
+                log.warn("No SecurityContext found in Redis session.");
+            } // if - else
+            
         } else {
             log.warn("No session found in Redis for sessionId: " + sessionId);
         } // if
