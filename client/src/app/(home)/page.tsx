15831180322b7275cc165778c0035b35fa26ae27
edel1212 <50935771+edel1212@users.noreply.github.com>
2024-10-22 18:34:10 +0900

    Redis Header 방식 연결

--- client/src/app/(home)/page.tsx
+++ client/src/app/(home)/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import { useEffect, useState } from "react";
 
 export default function Home() {
   return (
@@ -13,6 +12,9 @@ export default function Home() {
         Redis Session 방식 - Cookie 사용
       </Link>
       <hr />
+      <Link href={"/redis-header-session"}>
+        Redis Session 방식 - Header 사용
+      </Link>
     </main>
   );
 }
