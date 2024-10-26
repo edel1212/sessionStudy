da18ed0b87554f31f4948345e3993b285a439039
yoo <edel1212@naver.com>
2024-10-26 22:27:17 +0900

    Cannot deserialize 삽질 중 ..

--- redisSession/server-cookie/src/main/java/com/yoo/server/config/RedisConfig.java
+++ redisSession/server-cookie/src/main/java/com/yoo/server/config/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.session.data.redis.RedisIndexedSessionRepository;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
@@ -16,13 +17,16 @@ public class RedisConfig {
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
-        redisTemplate.setKeySerializer(new StringRedisSerializer());  // 키를 문자열로 직렬화
-        redisTemplate.setValueSerializer(new StringRedisSerializer());  // 값도 문자열로 직렬화
+        redisTemplate.setKeySerializer(new StringRedisSerializer());  // 키는 문자열로 직렬화
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));  // 값을 JSON 직렬화 사용
         return redisTemplate;
     }
 
     @Bean
     public RedisIndexedSessionRepository redisIndexedSessionRepository(RedisTemplate<String, Object> redisTemplate) {
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
         return new RedisIndexedSessionRepository(redisTemplate);
     }
+
+
 }
